1b754928a4d38b7a5a4e16be164e87cc8cb9e9eb
Sriram <61050961+Sriram-52@users.noreply.github.com>
2021-03-25 11:50:01 +0530

    Update Prg2.py

--- Assignement-6/Prg2.py
+++ Assignement-6/Prg2.py
@@ -1,13 +1,31 @@
-#lex_auth_01269442114344550475
+#lex_auth_01269442963365068878
 
-def is_palindrome(word):
-    return word.lower() == word[::-1].lower()
-    #Remove pass and write your logic here
+def find_factors(num):
+    #Accepts a number and returns the list of all the factors of a given number
+    factors = []
+    for i in range(2,(num+1)):
+        if(num%i==0):
+            factors.append(i)
+    return factors
 
+def is_prime(num, i):
+    #Accepts the number num and num/2 --> i and returns True if the number is prime ,else returns False
+    if(i==1):
+        return True
+    elif(num%i==0):
+        return False;
+    else:
+        return(is_prime(num,i-1))
 
-#Provide different values for word and test your program
-result = is_palindrome("MadAMa")
-if(result):
-    print("The given word is a Palindrome")
-else:
-    print("The given word is not a Palindrome")
+def find_largest_prime_factor(list_of_factors):
+    #Accepts the list of factors and returns the largest prime factor
+
+def find_f(num):
+    #Accepts the number and returns the largest prime factor of the number
+
+def find_g(num):
+    #Accepts the number and returns the sum of the largest prime factors of the 9 consecutive numbers starting from the given number
+
+#Note: Invoke function(s) from other function(s), wherever applicable.
+
+print(find_g(10))
