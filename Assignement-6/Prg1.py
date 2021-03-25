413448e2c6ca34894513d197de1e93e6cf3d154f
Sriram <61050961+Sriram-52@users.noreply.github.com>
2021-03-25 11:43:49 +0530

    Update Prg1.py

--- Assignement-6/Prg1.py
+++ Assignement-6/Prg1.py
@@ -1,12 +1,14 @@
 #lex_auth_01269442114344550475
 
 def is_palindrome(word):
-    return word.lower() == word[::-1].lower()
     #Remove pass and write your logic here
-
+    word = word.lower()
+    if len(word) < 2: return True
+    if word[0] != word[-1]: return False
+    return is_palindrome(word[1:-1])
 
 #Provide different values for word and test your program
-result = is_palindrome("MadAMa")
+result=is_palindrome("MadAMa")
 if(result):
     print("The given word is a Palindrome")
 else:
